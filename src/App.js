182783c74f74870f3729405a1517f482eb2b4c53
Luis Garcia <lg_20_GA@hotmail.com>
2023-11-29 00:43:37 -0500

    PROPS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import { Testimonios } from "./components/Testimonios";
 export function App() {
   return (
     <div className="App">
-      <Testimonios /> <Testimonios />
+      <h1>ESTO SON LAS LIBRERIAS Y FRAMEWORS MAS USADOS</h1>
+      <Testimonios
+        name="React "
+        post="Libreria"
+        testimonial="React es una biblioteca de JavaScript desarrollada por Facebook que se utiliza para construir interfaces de usuario interactivas y reactivas. Se enfoca en la creación de componentes reutilizables que gestionan su propio estado y se pueden componer para construir interfaces de usuario más complejas."
+        imagen="react"
+      />
+      <Testimonios
+        name="Angular "
+        post="Framework"
+        testimonial="Angular es un framework de desarrollo web y aplicación de código abierto mantenido por Google y una comunidad de desarrolladores. Angular se utiliza para construir aplicaciones web de una sola página (SPA) y aplicaciones web empresariales más complejas."
+        imagen="angular"
+      />
+      <Testimonios
+        name="Vue "
+        post="Framework"
+        testimonial="
+        Vue.js, comúnmente conocido como Vue, es un framework progresivo de JavaScript utilizado para construir interfaces de usuario (UI) y aplicaciones de una sola página (SPA). Fue creado por Evan You y lanzado por primera vez en 2014. Vue se destaca por su enfoque incremental, lo que significa que puedes adoptar y utilizar tanto o tan poco de Vue como desees en un proyecto existente."
+        imagen="vue"
+      />
     </div>
   );
 }
--- src/components/Testimonios.jsx
+++ src/components/Testimonios.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 
-export function Testimonios() {
+export function Testimonios(props) {
   return (
     <div className="container__testimonials">
       <img
         className="image__testimonials"
-        src={require("../images/react.png")}
+        src={require(`../images/${props.imagen}.png`)}
         alt="foto react"
       />
       <div className="container__text--testimonials">
-        <p className="name__testimonial">REACT</p>
-        <p className="post__testimonial">Libreria React</p>
-        <p className="txt__testimonial">
-        React es una biblioteca de JavaScript desarrollada por 
-        Facebook que se utiliza para construir interfaces de 
-        usuario interactivas y reactivas. Se enfoca en la creación 
-        de componentes reutilizables que gestionan su propio 
-        estado y se pueden componer para construir interfaces 
-        de usuario más complejas.
-        </p>
+        <p className="name__testimonial">{props.name}</p>
+        <p className="post__testimonial">{props.post}</p>
+        <p className="txt__testimonial">{props.testimonial}</p>
       </div>
     </div>
   );
